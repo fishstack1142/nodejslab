b4588b15c6a24a268653a840c07770504a68fb14
fishstack1142 <fishstack1142@gmail.com>
2019-09-29 21:45:15 +0700

    use promise

--- callback_to_promise.js
+++ callback_to_promise.js
@@ -2,13 +2,13 @@
 
 console.log('Before');
 
-// getUser(1, (user) => {
-//   getRepositories(user.gitHubUsername, (repos) => {
-//     getCommits(repos[0], (commits) => {
-//       console.log(commits);
-//     })
-//   })
-// });
+getUser(1, (user) => {
+  getRepositories(user.gitHubUsername, (repos) => {
+    getCommits(repos[0], (commits) => {
+      console.log(commits);
+    })
+  })
+});
 
 getUser(1).then(user => getRepositories(user.username))
 //then it return promise
@@ -16,6 +16,21 @@ getUser(1).then(user => getRepositories(user.username))
 .then(commits => console.log('Commits :', commits))
 .catch(err => console.log('Error :', err.message))
 ;
+
+// async function displayCommits() {
+//     try{
+//         const user = await getUser(1);
+//         const repos = await getRepositories(user.username);
+//         const commits = await getCommits(repos[0]);
+//         console.log(commits);
+
+//     } catch (err) {
+//         console.log('Error : ',  err.message);
+//     }
+
+// }
+
+// displayCommits();
 
 console.log('After');
 
